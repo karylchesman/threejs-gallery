a902077f73ad59b038a3f800cd36fac14fb25f7e
Karyl Chesman <karylbps@gmail.com>
2025-03-11 20:02:14 -0300

    feat: implemented the basic three usage

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,31 @@
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>Hello, world!</h1>
-  </div>
-`;
+import * as THREE from "three";
+import "./index.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+app.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
